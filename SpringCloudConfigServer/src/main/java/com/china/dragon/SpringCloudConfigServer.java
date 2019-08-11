462905c03688dcd13ff02a2763f989a2f9aae5b0
shutao <shutaolucky@sina.com>
2019-08-11 18:22:00 +0800

    Bug:20190811 Bugzilla Project:Dragon Cloud Description:Project initialization

---
+++ SpringCloudConfigServer/src/main/java/com/china/dragon/SpringCloudConfigServer.java
@@ -0,0 +1,18 @@
+package com.china.dragon;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author: henry
+ * @Date: 2019/8/11 0011 下午 17:04
+ * @Desc
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServer {
+
+    public static void main(String[] args) {
+
+    }
+}
