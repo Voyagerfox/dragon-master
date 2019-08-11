cfebf847cf05dfc35608558717a47550c030d682
shutao <shutaolucky@sina.com>
2019-08-11 21:58:02 +0800

    Bug:20190811 Bugzilla Project:Dragon Cloud Description:add conusl

--- SpringCloudConfigServer/src/main/java/com/china/dragon/SpringCloudConfigServer.java
+++ SpringCloudConfigServer/src/main/java/com/china/dragon/SpringCloudConfigServer.java
@@ -1,6 +1,9 @@
 package com.china.dragon;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
@@ -8,11 +11,13 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  * @Date: 2019/8/11 0011 下午 17:04
  * @Desc
  */
+@EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
-public class SpringCloudConfigServer {
+public class SpringCloudConfigServer extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigServer.class,args);
 
     }
 }
