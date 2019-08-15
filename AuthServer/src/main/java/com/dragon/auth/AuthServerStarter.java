33404b627764227c91d54ddfb880e58937133281
shutao <shutaolucky@sina.com>
2019-08-16 00:02:09 +0800

    Bug:20190815 Bugzilla Project: Dragon Cloud Description:modify ymal file

--- AuthServer/src/main/java/com/dragon/auth/AuthServerStarter.java
+++ AuthServer/src/main/java/com/dragon/auth/AuthServerStarter.java
@@ -2,6 +2,7 @@ package com.dragon.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
@@ -11,7 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class AuthServerStarter {
+public class AuthServerStarter extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthServerStarter.class,args);
